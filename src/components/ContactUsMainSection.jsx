a2cadb067713f59938d522c42244aac46e953bb5
phwnganh <phuonganh25102003@gmail.com>
2026-03-07 18:51:15 +0700

    feat: done hover items

--- src/components/ContactUsMainSection.jsx
+++ src/components/ContactUsMainSection.jsx
@@ -11,7 +11,7 @@ const ContactUsMainSection = () => {
                 <input type={"checkbox"} required/>
                 <label htmlFor={"confirmation-info"} className={"text-body-sm leading-body text-grey-900"}>I consent to being contacted by the team <span className={"text-green-600"}>*</span></label>
             </div>
-            <button type={"submit"} aria-labelledby={"submit-button"} className={"text-white text-body-md font-body leading-body px-10 py-4 rounded-lg bg-green-600"}>Submit</button>
+            <button type={"submit"} aria-labelledby={"submit-button"} className={"text-white text-body-md font-body leading-body px-10 py-4 rounded-lg bg-green-600 hover:bg-[linear-gradient(#00000080,#00000080)]"}>Submit</button>
         </form>
     );
 };
