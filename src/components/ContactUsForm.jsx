472ed5ce37b0049a82c9445b4fa4eafaed660c5d
phwnganh <phuonganh25102003@gmail.com>
2026-03-07 19:04:43 +0700

    fix: active to new bg color when clicking radio input

--- src/components/ContactUsForm.jsx
+++ src/components/ContactUsForm.jsx
@@ -21,13 +21,13 @@ const ContactUsForm = () => {
             <div className={"flex flex-col gap-2"}>
                     <legend className={"text-grey-900 text-body-sm leading-body"}>Query Type <span className={"text-green-600"}>*</span></legend>
                     <div className={"flex flex-col sm:flex-row gap-4"}>
-                        <div className={"w-full flex gap-3 items-center rounded-lg border border-grey-500 px-6 py-3 hover:outline-green-600 hover:outline-1 active:bg-green-200"}>
-                            <input type={"radio"} className={""} required id={"generalEnquiry"} name={"generalEnquiry"} aria-describedby={"Tick the general enquiry"}/>
+                        <div className={"w-full flex gap-3 items-center rounded-lg border border-grey-500 px-6 py-3 hover:outline-green-600 hover:outline-1 has-checked:bg-green-200"}>
+                            <input type={"radio"} className={"accent-green-600 w-6 h-6"} required id={"generalEnquiry"} name={"queryType"} aria-describedby={"Tick the general enquiry"}/>
                             <label htmlFor={"generalEnquiry"} className={"text-grey-900 text-body-md leading-body"}>General Enquiry</label>
                         </div>
 
-                        <div className={"w-full flex gap-3 items-center rounded-lg border border-grey-500 px-6 py-3 hover:outline-green-600 hover:outline-1 active:bg-green-200"}>
-                            <input type={"radio"} className={""} required id={"supportRequest"} name={"supportRequest"} aria-describedby={"Tick for you supporting request"}/>
+                        <div className={"w-full flex gap-3 items-center rounded-lg border border-grey-500 px-6 py-3 hover:outline-green-600 hover:outline-1 has-checked:bg-green-200"}>
+                            <input type={"radio"} className={"accent-green-600 w-6 h-6"} required id={"supportRequest"} name={"queryType"} aria-describedby={"Tick for you supporting request"}/>
                             <label htmlFor={"supportRequest"} className={"text-grey-900 text-body-md leading-body"}>Support Request</label>
                         </div>
                     </div>
--- src/components/ContactUsMainSection.jsx
+++ src/components/ContactUsMainSection.jsx
@@ -8,7 +8,7 @@ const ContactUsMainSection = () => {
         <form method={"post"} onSubmit={handleSubmit} className={"flex flex-col gap-10"}>
             <ContactUsForm/>
             <div className={"flex items-center gap-4"}>
-                <input type={"checkbox"} required/>
+                <input type={"checkbox"} required className={"accent-green-600 w-6 h-6"}/>
                 <label htmlFor={"confirmation-info"} className={"text-body-sm leading-body text-grey-900"}>I consent to being contacted by the team <span className={"text-green-600"}>*</span></label>
             </div>
             <button type={"submit"} aria-labelledby={"submit-button"} className={"text-white text-body-md font-body leading-body px-10 py-4 rounded-lg bg-green-600 hover:bg-[linear-gradient(#00000080,#00000080)]"}>Submit</button>
